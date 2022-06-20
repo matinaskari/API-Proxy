fe7c4c97004944b6ac05ecefc297356ea8d87b92
matinaskari <askarimatin79@gmail.com>
2022-06-20 17:11:35 +0430

    add html form to set secret

--- build/routes/api.js
+++ build/routes/api.js
@@ -2,6 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var proxy_routes_1 = require("../routes/proxy.routes");
+var path_1 = require("path");
 var router = (0, express_1.Router)();
 router.use("/proxy", proxy_routes_1.router);
+router.get("/", function (req, res) {
+    res.sendFile((0, path_1.join)(__dirname + "../views/set-secret.html"));
+    //__dirname : It will resolve to your project folder.
+});
 exports.default = router;
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { router as proxyRoutes } from "../routes/proxy.routes";
+import { join } from "path";
 
 const router = Router();
 
 router.use("/proxy", proxyRoutes);
+
+router.get("/", function (req, res) {
+  res.sendFile("set-secret.html", {
+    root: join(__dirname, "../views"),
+  });
+});
 
 export default router;
