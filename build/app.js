2c6b3a82160b60c3bf8e6cb73fb6ed14c6c62f3f
matinaskari <askarimatin79@gmail.com>
2022-06-20 17:11:59 +0430

    bug fix

--- build/app.js
+++ build/app.js
@@ -7,12 +7,10 @@ var express_1 = __importDefault(require("express"));
 var config_1 = require("./config");
 var morgan_1 = __importDefault(require("morgan"));
 var api_1 = __importDefault(require("./routes/api"));
-var path_1 = require("path");
 var app = (0, express_1.default)();
 app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded());
-app.use(express_1.default.static((0, path_1.join)(__dirname, "public")));
 app.use("/api", api_1.default);
 app.listen(config_1.PORT, "127.0.0.1", function () {
     return console.log("[i] server is running on :".concat(config_1.PORT, " ..."));
--- build/routes/api.js
+++ build/routes/api.js
@@ -6,7 +6,8 @@ var path_1 = require("path");
 var router = (0, express_1.Router)();
 router.use("/proxy", proxy_routes_1.router);
 router.get("/", function (req, res) {
-    res.sendFile((0, path_1.join)(__dirname + "../views/set-secret.html"));
-    //__dirname : It will resolve to your project folder.
+    res.sendFile("set-secret.html", {
+        root: (0, path_1.join)(__dirname, "../views"),
+    });
 });
 exports.default = router;
